05a7483be3af7be00032864477d1427004b04ba5
Andrea Romanello <rommelandrea@gmail.com>
2012-07-25 21:35:41 +0200

    inserita stampa ordinata per pid (da testare che funzioni)

--- funzioni_frontend.c
+++ funzioni_frontend.c
@@ -14,3 +14,67 @@ void stampa(lista p) {
 	}else
 		printf("\n\n");
 }
+
+
+/**
+ * crea un nuovo elemento nella coda e ci inserisce il messaggio r
+ * @param r messaggio da inserire
+ * @return ritorna l'elemento creato
+ */
+coda * crea_nuovo(reservation *r) {
+	coda *c;
+	c = calloc(1, sizeof(coda));
+	c->messaggio = calloc(1, sizeof(reservation));
+	c->messaggio = r;
+	c->next = NULL;
+
+	return c;
+}
+
+/**
+ * inserisce un pessaggio r sulla coda c
+ * @param c coda sul quale fare l'inserimento
+ * @param r messaggio da inserire
+ * @return return della coda
+ */
+coda * insert_by_pid(coda *c, reservation *r) {
+	if (c == NULL ) {
+		return crea_nuovo(r);
+	} else {
+		if (c->next == NULL ) {
+			c->next = crea_nuovo(r);
+		} else {
+			if (c->messaggio->clientId > r->clientId) {
+				coda *tmp = crea_nuovo(r);
+				tmp->next = c;
+				return tmp;
+			} else {
+				if (c->next->messaggio->clientId < r->clientId)
+					inserisci(c->next, r);
+				else {
+					coda *tmp;
+					tmp = c->next;
+					c->next = crea_nuovo(r);
+					c->next->next = tmp;
+				}
+			}
+		}
+	}
+	return c;
+}
+
+/**
+ * stampa una copia della coda ordinata secondo il num di pid
+ * @param c coda originale
+ */
+void print_by_pid(coda * c){
+	coda * tmp = NULL;
+	while(c){
+		tmp = calloc(1, sizeof(coda));
+		tmp->messaggio = calloc(1, sizeof(reservation));
+		tmp = insert_by_pid(tmp, c->messaggio);
+		c = c->next;
+	}
+
+	stampa(tmp);
+}
